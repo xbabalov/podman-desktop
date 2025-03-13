66c6bbbdcb7fa7c154993cf186f0f0ed55e87e46
Evzen Gasta <evzen.ml@seznam.cz>
2025-03-13 12:54:19 +0100

    chore: added jobs
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- packages/renderer/src/lib/job/JobEmptyScreen.svelte
+++ packages/renderer/src/lib/job/JobEmptyScreen.svelte
@@ -3,4 +3,4 @@ import JobIcon from '../images/JobIcon.svelte';
 import KubernetesEmptyScreen from '../kube/KubernetesEmptyScreen.svelte';
 </script>
 
-<KubernetesEmptyScreen icon={JobIcon} title="No jobs" message="Try switching to a different context or namespace" />
+<KubernetesEmptyScreen icon={JobIcon} resources={['jobs']} titleEmpty={['No jobs']} titleNotPermitted={['Jobs not accessible']} message="Try switching to a different context or namespace" />
