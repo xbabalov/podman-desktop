0614fa379923f19380c174c59f7e90630da65355
xbabalov <t.babalova.17@gmail.com>
2025-03-18 12:55:08 +0100

    chore(test): add exact to settings bar link search
    Signed-off-by: xbabalov <t.babalova.17@gmail.com>

--- tests/playwright/src/model/pages/settings-bar.ts
+++ tests/playwright/src/model/pages/settings-bar.ts
@@ -59,7 +59,7 @@ export class SettingsBar {
   }
 
   public getPreferencesLinkLocator(label: string): Locator {
-    return this.settingsNavBar.getByRole('link', { name: label });
+    return this.settingsNavBar.getByRole('link', { name: label, exact: true });
   }
 
   public async expandPreferencesTab(): Promise<void> {
