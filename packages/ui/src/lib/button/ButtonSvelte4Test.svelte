cc37ab5dd37390856d6f631e8e3f47112e364507
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-04-08 14:15:38 +0200

    test(ui): adding svelte legacy usage for button (#12076)
    * test(ui): adding svelte legacy usage for button
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * test: adding icon coverage
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix(legacy-button): rename test as recommended by @benoitf
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ packages/ui/src/lib/button/ButtonSvelte4Test.svelte
@@ -0,0 +1,14 @@
+<!-- force the component to be legacy mode -->
+<svelte:options runes={false} />
+<script lang="ts">
+import { Button } from '/@/lib';
+
+export let onclick: () => void;
+export let content: string | undefined;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export let icon: any | undefined;
+</script>
+
+<Button on:click={onclick} icon={icon}>
+  {content}
+</Button>
