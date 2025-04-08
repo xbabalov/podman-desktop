a7a523867a3389d7e4fb38ba1f834b5553f60d69
xbabalov <t.babalova.17@gmail.com>
2025-04-08 12:56:10 +0200

    chore(test): rework resource machine tests
    Signed-off-by: xbabalov <t.babalova.17@gmail.com>

--- tests/playwright/src/specs/podman-machine-resources.spec.ts
+++ tests/playwright/src/specs/podman-machine-resources.spec.ts
@@ -18,8 +18,9 @@
 
 import type { Locator } from '@playwright/test';
 
+import { ResourceElementActions } from '../model/core/operations';
+import { ResourceElementState } from '../model/core/states';
 import { CreateMachinePage } from '../model/pages/create-machine-page';
-import { PodmanMachineDetails } from '../model/pages/podman-machine-details-page';
 import { ResourceConnectionCardPage } from '../model/pages/resource-connection-card-page';
 import { ResourcesPage } from '../model/pages/resources-page';
 import { expect as playExpect, test } from '../utility/fixtures';
@@ -30,10 +31,9 @@ import {
   handleConfirmationDialog,
 } from '../utility/operations';
 import { isLinux, isWindows } from '../utility/platform';
-import { waitForPodmanMachineStartup } from '../utility/wait';
+import { waitForPodmanMachineStartup, waitUntil } from '../utility/wait';
 
-const DEFAULT_PODMAN_MACHINE = 'Podman Machine';
-const DEFAULT_PODMAN_MACHINE_VISIBLE = 'podman-machine-default';
+const DEFAULT_PODMAN_MACHINE_NAME = 'podman-machine-default';
 const RESOURCE_NAME = 'podman';
 let dialog: Locator;
 
@@ -58,7 +58,55 @@ const machineTypes = [
   },
 ];
 
+test.beforeAll(async ({ runner, welcomePage, page, navigationBar }) => {
+  runner.setVideoAndTraceName(`podman-machine-resources-e2e`);
+
+  await welcomePage.handleWelcomePage(true);
+  await waitForPodmanMachineStartup(page);
+  dialog = page.getByRole('dialog', { name: 'Podman', exact: true });
+
+  // Check for default machine and stop
+  const settingsBar = await navigationBar.openSettings();
+  await settingsBar.resourcesTab.click();
+
+  const resourcesPage = new ResourcesPage(page);
+  await playExpect.poll(async () => await resourcesPage.resourceCardIsVisible(RESOURCE_NAME)).toBeTruthy();
+  const defaultMachineCard = new ResourceConnectionCardPage(page, RESOURCE_NAME, DEFAULT_PODMAN_MACHINE_NAME);
+
+  playExpect(await defaultMachineCard.resourceElementConnectionStatus.innerText()).toContain(
+    ResourceElementState.Running,
+  );
+  await defaultMachineCard.performConnectionAction(ResourceElementActions.Stop);
+  await waitUntil(
+    async () =>
+      (await defaultMachineCard.resourceElementConnectionStatus.innerText()).includes(ResourceElementState.Off),
+    { timeout: 30_000, sendError: true },
+  );
+});
+
+test.afterAll(async ({ runner, page }) => {
+  test.setTimeout(120_000);
+
+  await createPodmanMachineFromCLI();
+
+  try {
+    await handleConfirmationDialog(page, 'Podman', true, 'Yes');
+    await handleConfirmationDialog(page, 'Podman', true, 'OK');
+  } catch (error) {
+    console.log('No handling dialog displayed', error);
+  }
+
+  await runner.close();
+});
+
 for (const { PODMAN_MACHINE_NAME, MACHINE_VISIBLE_NAME, isRoot, userNet } of machineTypes) {
+  test.afterAll(async () => {
+    test.setTimeout(60_000);
+    if (test.info().status === 'failed') {
+      await deletePodmanMachineFromCLI(PODMAN_MACHINE_NAME);
+    }
+  });
+
   test.skip(
     isLinux || process.env.TEST_PODMAN_MACHINE !== 'true',
     'Tests suite should not run on Linux platform or if TEST_PODMAN_MACHINE is not true',
@@ -69,67 +117,15 @@ for (const { PODMAN_MACHINE_NAME, MACHINE_VISIBLE_NAME, isRoot, userNet } of mac
     'Testing user networking machine only on Windows',
   );
 
-  test.beforeAll(async ({ runner, welcomePage, page }) => {
-    runner.setVideoAndTraceName(`${PODMAN_MACHINE_NAME}-resources-e2e`);
-    process.env.KEEP_TRACES_ON_PASS = 'true';
-
-    await welcomePage.handleWelcomePage(true);
-    await waitForPodmanMachineStartup(page);
-    dialog = page.getByRole('dialog', { name: 'Podman', exact: true });
-  });
-
-  test.afterAll(async ({ runner, page }) => {
-    test.setTimeout(120_000);
-
-    if (test.info().status === 'failed') {
-      await deletePodmanMachineFromCLI(PODMAN_MACHINE_NAME);
-      await createPodmanMachineFromCLI();
-    }
-
-    try {
-      await handleConfirmationDialog(page, 'Podman', true, 'Yes');
-      await handleConfirmationDialog(page, 'Podman', true, 'OK');
-    } catch (error) {
-      console.log('No handling dialog displayed', error);
-    }
-
-    await runner.close();
-  });
-
   test.describe
-    .serial(`${MACHINE_VISIBLE_NAME} Resources creation Verification`, () => {
-      test('Check data for available Podman Machine and stop machine', async ({ page, navigationBar }) => {
-        test.setTimeout(120_000);
-        const settingsBar = await navigationBar.openSettings();
-        await settingsBar.resourcesTab.click();
-
-        const resourcesPage = new ResourcesPage(page);
-        await playExpect(resourcesPage.heading).toBeVisible();
-        await playExpect.poll(async () => await resourcesPage.resourceCardIsVisible(RESOURCE_NAME)).toBeTruthy();
-        const resourcesPodmanConnections = new ResourceConnectionCardPage(
-          page,
-          RESOURCE_NAME,
-          DEFAULT_PODMAN_MACHINE_VISIBLE,
-        );
-
-        await playExpect(resourcesPodmanConnections.resourceElementDetailsButton).toBeVisible({ timeout: 10_000 });
-        await resourcesPodmanConnections.resourceElementDetailsButton.click();
-
-        const podmanMachineDetails = new PodmanMachineDetails(page, DEFAULT_PODMAN_MACHINE);
-
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('RUNNING', { timeout: 60_000 });
-        await playExpect(podmanMachineDetails.podmanMachineStopButton).toBeEnabled();
-        await podmanMachineDetails.podmanMachineStopButton.click();
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('OFF', { timeout: 60_000 });
-      });
-
-      test('Create a machine through Resources page', async ({ page, navigationBar }) => {
+    .serial(`${MACHINE_VISIBLE_NAME} Resources workflow Verification`, () => {
+      test('Create machine through Resources page', async ({ page, navigationBar }) => {
         test.setTimeout(200_000);
 
         const settingsBar = await navigationBar.openSettings();
         await settingsBar.resourcesTab.click();
 
-        const podmanResources = new ResourceConnectionCardPage(page, 'podman');
+        const podmanResources = new ResourceConnectionCardPage(page, RESOURCE_NAME);
         await podmanResources.createButton.click();
 
         const createMachinePage = new CreateMachinePage(page);
@@ -141,59 +137,44 @@ for (const { PODMAN_MACHINE_NAME, MACHINE_VISIBLE_NAME, isRoot, userNet } of mac
         });
 
         await playExpect(resourcePage.heading).toBeVisible();
-        const machineBox = new ResourceConnectionCardPage(page, 'podman', PODMAN_MACHINE_NAME); //does not work with visible name
+        const machineCard = new ResourceConnectionCardPage(page, RESOURCE_NAME, PODMAN_MACHINE_NAME);
+        playExpect(await machineCard.doesResourceElementExist()).toBeTruthy();
+        playExpect(await machineCard.resourceElementConnectionStatus.innerText()).toContain(ResourceElementState.Off);
+      });
 
-        await playExpect(machineBox.resourceElementDetailsButton).toBeVisible({ timeout: 30_000 });
-        await machineBox.resourceElementDetailsButton.click();
-
-        const podmanMachineDetails = new PodmanMachineDetails(page, MACHINE_VISIBLE_NAME);
-        await playExpect(podmanMachineDetails.podmanMachineName).toBeVisible({ timeout: 30_000 });
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('OFF');
-
-        await playExpect(podmanMachineDetails.podmanMachineStartButton).toBeEnabled();
-        await podmanMachineDetails.podmanMachineStartButton.click();
+      test('Start the machine', async ({ page }) => {
+        const machineCard = new ResourceConnectionCardPage(page, RESOURCE_NAME, PODMAN_MACHINE_NAME);
+        await machineCard.performConnectionAction(ResourceElementActions.Start);
 
         await playExpect(dialog).toBeVisible({ timeout: 60_000 });
         await handleConfirmationDialog(page, 'Podman', true, 'Yes');
         await handleConfirmationDialog(page, 'Podman', true, 'OK');
 
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('RUNNING', { timeout: 90_000 });
-
-        await playExpect(podmanMachineDetails.podmanMachineStopButton).toBeEnabled();
-        await podmanMachineDetails.podmanMachineStopButton.click();
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('OFF', { timeout: 60_000 });
-      });
-
-      test('Restart default podman machine', async ({ page, navigationBar }) => {
-        const dashboard = await navigationBar.openDashboard();
-        await playExpect(dashboard.heading).toBeVisible();
-        const settingsBar = await navigationBar.openSettings();
-        await settingsBar.resourcesTab.click();
-
-        const resourcesPage = new ResourcesPage(page);
-        await playExpect(resourcesPage.heading).toBeVisible();
-        await playExpect.poll(async () => await resourcesPage.resourceCardIsVisible(RESOURCE_NAME)).toBeTruthy();
-        const resourcesPodmanConnections = new ResourceConnectionCardPage(
-          page,
-          RESOURCE_NAME,
-          DEFAULT_PODMAN_MACHINE_VISIBLE,
+        await waitUntil(
+          async () =>
+            (await machineCard.resourceElementConnectionStatus.innerText()).includes(ResourceElementState.Running),
+          { timeout: 30_000, sendError: true },
         );
-        await playExpect(resourcesPodmanConnections.resourceElementDetailsButton).toBeVisible();
-        await resourcesPodmanConnections.resourceElementDetailsButton.click();
-
-        const podmanMachineDetails = new PodmanMachineDetails(page, DEFAULT_PODMAN_MACHINE);
-
-        await playExpect(podmanMachineDetails.podmanMachineStartButton).toBeEnabled();
-        await podmanMachineDetails.podmanMachineStartButton.click();
-
-        await playExpect(dialog).toBeVisible({ timeout: 60_000 });
-        await handleConfirmationDialog(page, 'Podman', true, 'Yes');
-        await handleConfirmationDialog(page, 'Podman', true, 'OK');
-
-        await playExpect(podmanMachineDetails.podmanMachineStatus).toHaveText('RUNNING', { timeout: 90_000 });
       });
 
-      test('Clean up test machine', async ({ page }) => {
+      test('Restart the machine', async ({ page }) => {
+        const machineCard = new ResourceConnectionCardPage(page, RESOURCE_NAME, PODMAN_MACHINE_NAME);
+        await machineCard.performConnectionAction(ResourceElementActions.Restart);
+
+        await waitUntil(
+          async () =>
+            (await machineCard.resourceElementConnectionStatus.innerText()).includes(ResourceElementState.Off),
+          { timeout: 30_000, sendError: true },
+        );
+
+        await waitUntil(
+          async () =>
+            (await machineCard.resourceElementConnectionStatus.innerText()).includes(ResourceElementState.Running),
+          { timeout: 30_000, sendError: true },
+        );
+      });
+
+      test('Stop and delete the machine', async ({ page }) => {
         test.setTimeout(150_000);
         await deletePodmanMachine(page, PODMAN_MACHINE_NAME);
 
@@ -201,7 +182,7 @@ for (const { PODMAN_MACHINE_NAME, MACHINE_VISIBLE_NAME, isRoot, userNet } of mac
           await handleConfirmationDialog(page, 'Podman', true, 'Yes');
           await handleConfirmationDialog(page, 'Podman', true, 'OK');
         } catch (error) {
-          console.log('No handing dialog displayed', error);
+          console.log('No handling dialog displayed', error);
         }
       });
     });
