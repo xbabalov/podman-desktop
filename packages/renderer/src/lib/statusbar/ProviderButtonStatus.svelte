114f04de92e2bbc6d2e455a3555f76e3c93d24ce
Sonia Sandler <66797193+SoniaSandler@users.noreply.github.com>
2025-04-14 11:19:02 -0400

    chore: add error indication to statusbar provider (#12191)
    chore: add error indication
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- packages/renderer/src/lib/statusbar/ProviderButtonStatus.svelte
+++ packages/renderer/src/lib/statusbar/ProviderButtonStatus.svelte
@@ -15,6 +15,8 @@ let { status, class: className = '' }: Props = $props();
   <div aria-label="Connection Status Icon" class="h-3 w-3 rounded-full animate-spin border border-solid border-[var(--pd-action-button-spinner)] border-t-transparent {className}"></div>
 {:else if status === 'ready' || status === 'started'}
   <div aria-label="Connection Status Icon" class="h-[7px] w-[7px] absolute top-[0px] right-[-2px] rounded-full bg-[var(--pd-status-contrast)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
+{:else if status === 'error'}
+  <div aria-label="Connection Status Icon" class="h-[7px] w-[7px] absolute top-[0px] right-[-2px] rounded-full bg-[var(--pd-state-error)] border-1 border-[var(--pd-statusbar-bg)] {className}"></div>
 {:else if status === 'Update available'}
   <IconImage image={arrowUp} aria-label="Connection Status Icon" class="h-[12px] w-auto absolute top-0 right-[-30%] {className}" />
 {/if}
