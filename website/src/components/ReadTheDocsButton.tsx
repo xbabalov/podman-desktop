99882a7dbca4192ff8624f52de6f852596e066d9
Evzen Gasta <evzen.ml@seznam.cz>
2025-04-24 14:31:39 +0200

    chore(webiste): updated size and bg
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- website/src/components/ReadTheDocsButton.tsx
+++ website/src/components/ReadTheDocsButton.tsx
@@ -4,7 +4,7 @@ export function ReadTheDocsButton(): JSX.Element {
   return (
     <div>
       <Link
-        className="no-underline hover:no-underline inline-flex text-white font-bold hover:text-white bg-purple-700 border-0 py-2 px-4 mt-6 mb-1 focus:outline-hidden hover:bg-purple-800 rounded-lg text-lg"
+        className="no-underline hover:no-underline inline-flex text-white font-bold hover:text-white bg-purple-600 border-0 py-2 px-4 mt-6 mb-1 focus:outline-hidden hover:bg-purple-700 rounded-lg text-base"
         to="/docs/intro">
         Read the Docs
       </Link>
