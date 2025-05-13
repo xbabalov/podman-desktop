8bca1509f98c84728e6150d334216459039c032f
Evzen Gasta <evzen.ml@seznam.cz>
2025-05-13 07:46:38 +0200

    chore(website): added option to define link in read the docs button
    Signed-off-by: Evzen Gasta <evzen.ml@seznam.cz>

--- website/src/components/ReadTheDocsButton.tsx
+++ website/src/components/ReadTheDocsButton.tsx
@@ -1,11 +1,11 @@
 import Link from '@docusaurus/Link';
 
-export function ReadTheDocsButton(): JSX.Element {
+export function ReadTheDocsButton(props: Readonly<{ to: string }>): JSX.Element {
   return (
     <div>
       <Link
         className="no-underline hover:no-underline inline-flex text-white font-bold hover:text-white bg-purple-600 border-0 py-2 px-4 mt-6 mb-1 focus:outline-hidden hover:bg-purple-700 rounded-lg text-base"
-        to="/docs/intro">
+        to={props.to}>
         Read the Docs
       </Link>
     </div>
