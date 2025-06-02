0143c5ac5a9162c80dc3faf7cbcc312b92be1392
axel7083 <42176370+axel7083@users.noreply.github.com>
2025-06-02 11:26:53 +0200

    chore(deps): bump electron-builder to v26 (#12351)
    * chore(deps): bump electron-builder
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: using 26.0.13
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    * fix: pnpm-lock
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

--- .electron-builder.config.cjs
+++ .electron-builder.config.cjs
@@ -146,7 +146,16 @@ const config = {
         arch: ['x64', 'arm64'],
       },
     ],
-    sign: configuration => azureCodeSign(configuration.path),
+    /**
+     * TODO: replace with {@link import('electron-builder').Configuration#win#azureSignOptions}
+     * references:
+     * - https://www.electron.build/code-signing-win#using-azure-trusted-signing-beta
+     * - https://github.com/electron-userland/electron-builder/releases/tag/v26.0.0
+     * - https://github.com/electron-userland/electron-builder/pull/8582
+     */
+    signtoolOptions: {
+      sign: configuration => azureCodeSign(configuration.path),
+    },
   },
   flatpak: {
     license: 'LICENSE',
@@ -247,6 +256,11 @@ if (process.env.APPLE_TEAM_ID) {
   };
 }
 
+/**
+ * @deprecated use {@link import('electron-builder').Configuration#win#azureSignOptions}
+ * @param filePath
+ * @return {Promise<void>}
+ */
 const azureCodeSign = filePath => {
   if (!process.env.AZURE_KEY_VAULT_URL) {
     console.log('Skipping code signing, no environment variables set for that.');
